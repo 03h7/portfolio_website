41dbae84f81a89b0994ebe06d14edc0f87f51632
03h7 <t.merlay@gmail.com>
2022-02-19 23:19:33 +0100

    1st version of main page

--- jquery.js
+++ jquery.js
@@ -1,3 +1,6 @@
+let colors = ["#3F84E5","#B20D30","#C17817","#753E7B"]
+let i = 0;
+
 $(function(){
     $(".twitter").on("mouseenter",
         function(){
@@ -27,4 +30,57 @@ $(function(){
         function(){
             $(".messgit").hide();
         });
+    $("#mail").on("mouseenter",
+        function(){
+            $(".messmail").show();
+        }).on("mouseleave",
+        function(){
+            $(".messmail").hide();
+        });
+
+    $(".first-el").on("mouseenter",
+        function(){
+            //var rand1 = colors[Math.floor(Math.random() * colors.length)];
+            $(".first-el").css("background",colors[3])
+        }).on("mouseleave",
+            function(){
+            $(".first-el").css("background", "#0b1a18")
+        });
+    $(".second-el").on("mouseenter",
+        function(){
+            //var rand2 = colors[Math.floor(Math.random() * colors.length)];
+            $(".second-el").css("background",colors[0])
+        }).on("mouseleave",
+            function(){
+            $(".second-el").css("background", "#0b1a18")
+        });
+    $(".third-el").on("mouseenter",
+        function(){
+            //var rand3 = colors[Math.floor(Math.random() * colors.length)];
+            $(".third-el").css("background",colors[2])
+        }).on("mouseleave",
+            function(){
+            $(".third-el").css("background", "#0b1a18")
+        });
+    $(".fourth-el").on("mouseenter",
+        function(){
+            //var rand4 = colors[Math.floor(Math.random() * colors.length)];
+            $(".fourth-el").css("background",colors[3])
+        }).on("mouseleave",
+            function(){
+            $(".fourth-el").css("background", "#0b1a18")
+        });
+});
+
+let mailEl = document.getElementById("mail");
+
+mailEl.addEventListener("click", function(){
+    var text = "theome.pro@gmail.com"
+    navigator.clipboard.writeText(text).then(function() {
+      }, function(err) {
+      });
+});
+
+$("#mail").on("click", function(){
+    $("#mail").toggleClass("flash")
 });
